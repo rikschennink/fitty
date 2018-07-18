d55a490afd83d5fca8f0c93b035d56e8a9f11e3a
Mikhail Khazov <mikhail.khazov@techops.ru>
2018-07-18 14:54:43 +0300

    add typings

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+declare module 'fitty' {
+  interface FittyOptions {
+    minSize?: number
+    maxSize?: number
+    multiLine?: boolean
+    observeMutations?: MutationObserverInit
+  }
+
+  function fitty (el: HTMLElement, options?: FittyOptions): FittyInstance
+  function fitty (el: string, options?: FittyOptions): FittyInstance[]
+  export default fitty
+
+  export interface FittyInstance {
+    fit: () => void
+    unsubscribe: () => void
+  }
+}
